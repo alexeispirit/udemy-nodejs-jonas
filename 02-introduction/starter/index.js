f18d7195369d3d7b7095cac7fca0c103575626d0
alexeispirit <alexeispirit@gmail.com>
2019-07-01 11:50:08 +0500

    013-building-a-(very)-simple-api

--- 02-introduction/starter/index.js
+++ 02-introduction/starter/index.js
@@ -30,6 +30,8 @@ const url = require('url');
 // console.log('Will read file!')
 
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
@@ -38,6 +40,11 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the product');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
