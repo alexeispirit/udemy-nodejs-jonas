2de6186f868ba1637e08c4c99f4bbb2500cf281f
alexeispirit <alexeispirit@gmail.com>
2019-07-01 15:32:39 +0500

    020-Third-party-modules

--- 02-introduction/starter/index.js
+++ 02-introduction/starter/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 // FILES
@@ -37,6 +40,9 @@ const dataObj = JSON.parse(data);
 const templateOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
