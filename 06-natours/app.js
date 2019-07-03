33c33d81f9146d7064469a33f3d81074dd596a36
alexeispirit <alexeispirit@gmail.com>
2019-07-03 09:57:10 +0500

    055-handling-patch-requests

--- 06-natours/app.js
+++ 06-natours/app.js
@@ -68,6 +68,25 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find(el => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here'
+    }
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
