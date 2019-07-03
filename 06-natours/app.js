b0476440bc5a3e34ffbd5f658930e3f3f5039d25
alexeispirit <alexeispirit@gmail.com>
2019-07-03 15:05:14 +0500

    060-using-3rd-party-middleware

--- 06-natours/app.js
+++ 06-natours/app.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1. Middleware
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -19,6 +22,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2. Route handlers
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -111,6 +115,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3. Routes
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -122,6 +127,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4. Start server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
