9eb5fca7649775904db2274ec1e39f14f91f8c0c
alexeispirit <alexeispirit@gmail.com>
2019-07-08 09:59:49 +0500

    087-refactoring-for-mvc

--- 06-natours/controllers/tourController.js
+++ 06-natours/controllers/tourController.js
@@ -1,19 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`tour id is ${val}`);
-  if (!req.params.id - 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -30,16 +15,16 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
+    // results: tours.length,
     data: {
-      tours
+      // tours
     }
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find(el => el.id === id);
+  // const id = parseInt(req.params.id, 10);
+  // const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
     status: 'success',
@@ -50,27 +35,17 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      // tour: newTour
     }
-  );
+  });
 };
 
 exports.updateTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find(el => el.id === id);
+  // const id = parseInt(req.params.id, 10);
+  // const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
     status: 'success',
@@ -81,8 +56,8 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find(el => el.id === id);
+  // const id = parseInt(req.params.id, 10);
+  // const tour = tours.find(el => el.id === id);
 
   res.status(204).json({
     status: 'success',
