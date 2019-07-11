51f18de7276d8003d76ff98e498b65a0c036a1a9
alexeispirit <alexeispirit@gmail.com>
2019-07-11 15:52:00 +0500

    135-sending-emails-with-nodemailer

---
+++ 06-natours/utils/email.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+  // 1. create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+    // for GMAIL
+    // service: 'Gmail',
+    // auth: {
+    //   user: process.env.EMAIL_USERNAME,
+    //   pass: process.env.EMAIL_PASSWORD
+    // }
+    // activate in gmail "less secure app" option
+  });
+
+  // 2. define email options
+  const mailOptions = {
+    from: 'Alexei Spirit <alexeispirit@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  // 3. send email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
