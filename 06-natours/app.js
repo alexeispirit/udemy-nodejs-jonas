3af224a98bd8ad600e547b500b4d00f880b29e21
alexeispirit <alexeispirit@gmail.com>
2019-07-12 12:17:44 +0500

    144-data-sanitization

--- 06-natours/app.js
+++ 06-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// sanitize data against NoSQL query injection
+app.use(mongoSanitize());
+
+// sanitize data against XSS
+app.use(xss());
 
 // serve static files
 app.use(express.static(`${__dirname}/public`));
