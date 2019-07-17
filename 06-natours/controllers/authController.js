ab5ca05f54b3e34737cdcbed4ef10b71d228f23f
alexeispirit <alexeispirit@gmail.com>
2019-07-17 16:10:15 +0500

    190-logging-in-users-with-API

--- 06-natours/controllers/authController.js
+++ 06-natours/controllers/authController.js
@@ -75,6 +75,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
@@ -101,6 +103,34 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
   // 5. grant access to protected route
   req.user = currentUser;
+  next();
+});
+
+// only for rendered pages (no errors)
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  // 1. getting token and check if it exists
+  if (req.cookies.jwt) {
+    // 2. token verification
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+
+    // 3. check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+    // 4. check if user changed password after token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    // 5. there is a logged in user
+    res.locals.user = currentUser;
+    req.user = currentUser;
+    return next();
+  }
   next();
 });
 
---
+++ 06-natours/public/js/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import { login } from './login';
+import { displayMap } from './mapbox';
+
+// DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', event => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    event.preventDefault();
+    login(email, password);
+  });
+}
