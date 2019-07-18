45348b8e3c5019cef96762137bcf03e33863c628
alexeispirit <alexeispirit@gmail.com>
2019-07-18 15:20:28 +0500

    200-saving-image-name-to-database

--- 06-natours/controllers/userController.js
+++ 06-natours/controllers/userController.js
@@ -56,6 +56,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2. filtered out unwanted fields
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3. update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
